2bf1dd49df07bf4298bb7b8d51c7287698538a7e
Ian Rees <ian@ianrees.net>
2015-04-01 13:33:09 -0700

    General cleanup, added filter methods.

--- mzgtfs/entities.py
+++ mzgtfs/entities.py
@@ -1,7 +1,18 @@
-"""GTFS entities."""
+"""GTFS entities.
+
+Classes:
+  Entity - GTFS entity base class
+  Agency
+  Route
+  Trip
+  StopTime
+  Stop
+
+"""
 import collections
 
 import geom
+import util
 
 ##### Entities #####
 
@@ -11,24 +22,26 @@ class Entity(object):
   Parent-child relationships:
   
     Agency -> 
-      Routes -> 
-        Trips -> 
-          StopTimes -> 
-            Stops
+      Route -> 
+        Trip -> 
+          StopTime -> 
+            Stop
   
   """
   entity_type = None
 
   def __init__(self, data, feed=None):
     """Row data from DictReader, and reference to feed."""
-    # The row data
+    # The row data. 
+    # This is a collections.namedtuple, and is read-only.
     self.data = data
-    # Reference to GTFS Reader
+    # Reference to GTFS Reader.
     self.feed = feed
     # Relationships (e.g. trips, stop_times, ...)
     self.children = None  
     self.parents = None
   
+  # GTFS row data.
   def __getitem__(self, key):
     """Proxy to row data."""
     return getattr(self.data, key)
@@ -39,11 +52,25 @@ class Entity(object):
       return self[key]
     except AttributeError:
       return default
-
+  
+  # Name methods.
   def name(self):
-    """A reasonable display name for the entity."""
+    """A reasonable name for the entity."""
     raise NotImplementedError
-      
+  
+  def id(self):
+    """An internal GTFS identifier, e.g. route_id."""
+    raise NotImplementedError
+  
+  def feedid(self, feedid):
+    """A canonical Onestop-style ID for this entity."""
+    return '%s-%s-%s'%(
+      feedid,
+      self.entity_type,
+      self.id()
+    )
+
+  # Entity geometry.
   def point(self):
     """Return a point geometry for this entity."""
     raise NotImplementedError  
@@ -56,15 +83,15 @@ class Entity(object):
     """Return a GeoJSON-type geometry for this entity."""
     raise NotImplementedError
     
-  def feedid(self, feedid):
-    """Return an identifier for a GTFS entity."""
-    return 'f-%s-%s-%s'%(
-      feedid,
-      self.entity_type,
-      self.id()
-    )
-    
-  # Relationships
+  # Load / Dump.
+  @classmethod
+  def from_json(cls, data):
+    raise NotImplementedError
+  
+  def json(self):
+    raise NotImplementedError  
+
+  # Graph.
   def pclink(self, parent, child):
     """Create a parent-child relationship."""
     if parent.children is None:
@@ -74,7 +101,7 @@ class Entity(object):
     parent.children.add(child)
     child.parents.add(parent)
     
-  # Children
+  # ... children
   def add_child(self, child):
     """Add a child relationship."""
     self.pclink(self, child)
@@ -90,7 +117,7 @@ class Entity(object):
     """Read children from GTFS feed."""
     return set()
   
-  # Parents
+  # ... parents
   def add_parent(self, parent):
     """Add a parent relationship."""
     self.pclink(parent, self)
@@ -114,6 +141,7 @@ class Agency(Entity):
     return self.get('agency_name')
 
   def id(self):
+    # agency_id is optional.
     return self.get('agency_id') or self.get('agency_name')
 
   def point(self):
@@ -126,15 +154,15 @@ class Agency(Entity):
   def bbox(self):
     return geom.bbox(self.stops())
 
-  def geojson(self):
+  def json(self):
     return {
       'type': 'FeatureCollection',
       'name': self.name(),
       'properties': self.data,
       'bbox': self.bbox(),
       'geometry': self.geometry(),
-      'routes': [r.geojson() for r in self.routes()],
-      'features': [s.geojson() for s in self.stops()]
+      'routes': [r.json() for r in self.routes()],
+      'features': [s.json() for s in self.stops()]
     }
 
   def geometry(self):
@@ -146,20 +174,10 @@ class Agency(Entity):
       ]
     }    
 
-  # Agency methods.
-  @classmethod
-  def from_json(cls, filename):
-    """Load from a GeoJSON representation of agency."""
-    with open(filename) as f:
-      data = json.load(f)
-    agency = cls()
-    raise NotImplementedError
-    return agency
-
+  # Agency specific methods.
   def preload(self):
     """Pre-load routes, trips, stops, etc."""
     # First, load all routes.
-    print 'preload...'
     routes_by_id = {}
     for route in self.routes():
       routes_by_id[route.id()] = route
@@ -168,7 +186,7 @@ class Agency(Entity):
       route.add_parent(self)
     # Second, load all trips.
     trips_by_id = {}
-    for trip in self.feed.readiter('trips'):
+    for trip in self.feed.iterread('trips'):
       if trip['route_id'] in routes_by_id:
         trips_by_id[trip.id()] = trip
         # set directly
@@ -176,18 +194,16 @@ class Agency(Entity):
         trip.add_parent(routes_by_id[trip['route_id']])
     # Third, load all stops...
     stops_by_id = {}
-    for stop in self.feed.readiter('stops'):
+    for stop in self.feed.iterread('stops'):
       stops_by_id[stop.id()] = stop
     # Finally, load stop_times.
-    for stop_time in self.feed.readiter('stop_times'):
+    for stop_time in self.feed.iterread('stop_times'):
       if stop_time['trip_id'] in trips_by_id:
         # set directly
         stop_time.add_child(stops_by_id[stop_time['stop_id']])
         stop_time.add_parent(trips_by_id[stop_time['trip_id']])
-    print '...done'
   
-  # Agency routes.
-  routes = lambda self:self.get_children()
+  # Graph.
   def _read_children(self):
     # Are we the only agency in the feed?
     check = lambda r:True
@@ -195,16 +211,38 @@ class Agency(Entity):
       check = lambda r:(r.get('agency_id') == self.id())
     # Get the routes...    
     return set(
-      filter(check, self.feed.readiter('routes'))
+      filter(check, self.feed.iterread('routes'))
     )
   
-  # Agency full data.
+  def routes(self):
+    """Return all routes for this agency."""
+    return set(self.get_children()) # copy
+  
+  def route(self, id):
+    """Return a single route by ID."""
+    return util.filtfirst(self.routes(), id=id)
+  
   def trips(self):
     """Return all trips for this agency."""
     trips = set()
     for route in self.routes():
       trips |= route.trips()
     return trips
+  
+  def trip(self, id):
+    """Return a single trip by ID."""
+    return util.filtfirst(self.trips(), id=id)
+
+  def stops(self):
+    """Return all stops visited by trips for this agency."""
+    stops = set()
+    for stop_time in self.stop_times():
+      stops |= stop_time.stops()
+    return stops
+
+  def stop(self, id):
+    """Return a single stop by ID."""
+    return util.filtfirst(self.stops(), id=id)
     
   def stop_times(self):
     """Return all stop_times for this agency."""
@@ -213,13 +251,6 @@ class Agency(Entity):
       stop_times |= trip.stop_times()
     return stop_times
     
-  def stops(self):
-    """Return all stops visited by trips for this agency."""
-    stops = set()
-    for stop_time in self.stop_times():
-      stops |= stop_time.stops()
-    return stops
-        
 class Route(Entity):
   """GTFS Route Entity."""
   entity_type = 'r'
@@ -233,7 +264,7 @@ class Route(Entity):
   def bbox(self):
     return geom.bbox(self.stops())
 
-  def geojson(self):
+  def json(self):
     return {
       'type': 'Feature',
       'name': self.name(),
@@ -267,16 +298,19 @@ class Route(Entity):
       ]
     }
   
-  # Trips
-  trips = lambda self:self.get_children()
+  # Graph.
   def _read_children(self):
+    """Children: Trips"""
     return set([
       trip for trip
-      in self.feed.readiter('trips')
+      in self.feed.iterread('trips')
       if trip.get('route_id') == self.id()
     ])
 
-  # Serves
+  def trips(self):
+    """Return trips for this route."""
+    return set(self.get_children()) # copy
+
   def stops(self):
     """Return stops served by this route."""
     serves = set()
@@ -292,23 +326,37 @@ class Trip(Entity):
   def id(self):
     return self.get('trip_id')
   
-  # Stop times
-  stop_times = lambda self:self.get_children()
+  # Graph
   def _read_children(self):
     """Return stop_times for this trip."""
     return set([
       stop_time for stop_time
-      in self.feed.readiter('stop_times')
+      in self.feed.iterread('stop_times')
       if stop_time.get('trip_id') == self.id()
     ])
     
+  def stop_times(self):
+    return set(self.get_children()) # copy
+
+  # Trip methods.
   def stop_sequence(self):
     """Return the sorted StopTimes for this trip."""
     return sorted(
       self.stop_times(), 
       key=lambda x:int(x.get('stop_sequence'))
     )
+    
+class StopTime(Entity):
+  """GTFS Stop Time Entity."""
+  
+  def point(self):
+    # Ugly hack.
+    return list(self.children)[0].point()
 
+  # Graph
+  def stops(self):
+    return set(self.get_children())
+    
 class Stop(Entity):  
   """GTFS Stop Entity."""
   entity_type = 's'
@@ -326,7 +374,7 @@ class Stop(Entity):
     c = self.point()
     return [c[0], c[1], c[0], c[1]]
 
-  def geojson(self):
+  def json(self):
     return {
       'type': 'Feature',
       'name': self.name(),
@@ -341,20 +389,10 @@ class Stop(Entity):
       "coordinates": self.point(),
     }
 
-  # Routes
+  # Stop methods.
   def routes(self):
     serves = set()
     for stop_time in self.parents:
       for trip in stop_time.parents:
         serves |= trip.parents
     return serves
-
-class StopTime(Entity):
-  """GTFS Stop Time Entity."""
-
-  def stops(self):
-    return self.children
-    
-  def point(self):
-    # Ugly hack.
-    return list(self.children)[0].point()
