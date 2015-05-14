d7ff5a1bc3465b15d33de8d212afd4d74da22b42
Ian Rees <ian@ianrees.net>
2015-05-13 18:11:01 -0700

    Additional validation methods

--- mzgtfs/validation.py
+++ mzgtfs/validation.py
@@ -1,9 +1,77 @@
 """Validation exceptions and managers."""
+import pytz
 import traceback
 import contextlib
 
+import iso639
+import widetime
+
 def make_validator(validator=None):
   return validator or ValidationManager()
+
+def valid_color(color):
+  color = str(color).lower().strip()
+  if len(color) != 6:
+    return False
+  for j in [color[i:i+2] for i in range(0,5,2)]:
+    try:
+      j = int(j,16)
+    except ValueError, e:
+      return False
+  return True
+  
+def valid_url(url):
+  return url.startswith('http')
+
+def valid_tz(tz):
+  return pytz.timezone(tz)
+
+def valid_language(lang):
+  return iso639.get_language(lang)
+
+def valid_int(value, vmin=None, vmax=None):
+  if not value:
+    value = 0
+  try:
+    value = int(value)
+  except ValueError, e:
+    return False
+  if vmin is not None and value < vmin:
+    return False    
+  if vmax is not None and value > vmax:
+    return False
+  return True
+
+def valid_in(value, within):
+  return value in within
+
+def valid_bool(value):
+  if not value:
+    value = 0
+  try:
+    value = int(value)
+  except ValueError, e:
+    return False
+  return value in [0,1]
+
+def valid_widetime(value):
+  try:
+    widetime.WideTime.from_string(value)
+  except ValueError:
+    return False
+  return True
+
+def valid_point(point):
+  if len(point) != 2:
+    return False  
+  lon, lat = point
+  if not (-180 <= lon <= 180):
+    return False
+  if not (-90 <= lat <= 90):
+    return False
+  return True
+
+##### Validation Exceptions #####
 
 class ValidationException(Exception):
   def __init__(self, message, source=None):
@@ -21,6 +89,8 @@ class ValidationWarning(ValidationException):
 class ValidationInfo(ValidationException):
   """Validation info."""
   pass
+
+##### Validation Managers #####
 
 class ValidationManager(object):
   def __init__(self):
@@ -57,3 +127,4 @@ class ValidationReport(ValidationManager):
     if issubclass(etype, ValidationException):
       self.exceptions.append(etype(value, source=s))
       return True
+
